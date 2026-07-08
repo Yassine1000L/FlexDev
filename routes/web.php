bbefd3b4d8aba82ca3f8e87a11c8e1842a00f2df
Yassine1000L <yassneyas11@gmail.com>
2026-07-08 23:08:56 +0200

    Redirect /contact to /#contact

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [PageController::class, 'home'])->name('home');
 Route::get('diensten', [PageController::class, 'diensten'])->name('diensten');
 Route::get('projecten', [PageController::class, 'projecten'])->name('projecten');
-Route::get('contact', [PageController::class, 'contact'])->name('contact');
+Route::get('contact', function () { return redirect('/#contact'); })->name('contact');
 Route::post('contact', [PageController::class, 'contactStore'])->name('contact.store');
 Route::get('hoe-ik-werk', [PageController::class, 'hoeIkWerk'])->name('hoe-ik-werk');
 Route::get('waarom-flex-dev', [PageController::class, 'waarom'])->name('waarom');
